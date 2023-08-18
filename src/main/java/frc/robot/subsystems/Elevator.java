1a9aa1f3740f263bae2e747edbe4ec665ff7b030
Daniel Gorbatov <dzgorbatov@gmail.com>
2023-08-17 18:10:12 -0700

    elvator

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -1,9 +1,34 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXInvertType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+
+import SushiFrcLib.Motor.MotorHelper;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Elevator extends SubsystemBase {
+    private final WPI_TalonFX leftElevator;
+    private final WPI_TalonFX rightElavtor;
 
-    
-    
+    private static Elevator instance;
+
+    public static Elevator getInstance() {
+        if (instance == null) {
+            instance = new Elevator();
+        }
+
+        return instance;
+    }
+
+    private Elevator() {
+        leftElevator = MotorHelper.createFalconMotor(3, TalonFXInvertType.Clockwise);
+        rightElavtor = MotorHelper.createFalconMotor(20, TalonFXInvertType.CounterClockwise);
+    }
+
+    @Override
+    public void periodic() {
+        leftElevator.set(ControlMode.PercentOutput, 0.1);
+        rightElavtor.set(ControlMode.PercentOutput, 0.1);
+    }
 }
