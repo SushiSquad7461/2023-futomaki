4cd48d293617f2f7fc56d0d72c09b6f00a80f0aa
Daniel Gorbatov <dzgorbatov@gmail.com>
2023-10-21 10:29:46 -0700

    move dis gansta tye shi to base class

--- src/main/java/frc/robot/subsystems/Swerve.java
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -19,13 +19,9 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.kPorts;
 import frc.robot.Constants.kSwerve;
+import frc.robot.util.BaseSwerve;
 
-public class Swerve extends SubsystemBase {
-    private final SwerveModule[] swerveMods;
-    private final Gyro gyro;
-    private final SwerveOdom odom;
-    private final Field2d field;
-
+public class Swerve extends BaseSwerve {
     private static Swerve instance;
 
     private boolean locationLock;
@@ -41,21 +37,19 @@ public class Swerve extends SubsystemBase {
         return instance;
     }
 
-
-
     private Swerve() {
-        gyro = new Pigeon(kPorts.PIGEON_ID, kSwerve.GYRO_INVERSION, kPorts.CANIVORE_NAME);
-        gyro.zeroGyro();
-
-
-        swerveMods = new SwerveModuleNeoFalcon[]{
-            new SwerveModuleNeoFalcon(kSwerve.MOD0_CONSTANTS),
-            new SwerveModuleNeoFalcon(kSwerve.MOD1_CONSTANTS),
-            new SwerveModuleNeoFalcon(kSwerve.MOD2_CONSTANTS),
-            new SwerveModuleNeoFalcon(kSwerve.MOD3_CONSTANTS),
-        }; 
-
-        odom = new SwerveOdom(kSwerve.SWERVE_KINEMATICS, getPose());
+        super(
+            new SwerveModuleNeoFalcon[]{
+                new SwerveModuleNeoFalcon(kSwerve.MOD0_CONSTANTS),
+                new SwerveModuleNeoFalcon(kSwerve.MOD1_CONSTANTS),
+                new SwerveModuleNeoFalcon(kSwerve.MOD2_CONSTANTS),
+                new SwerveModuleNeoFalcon(kSwerve.MOD3_CONSTANTS),
+            },
+            new Pigeon(kPorts.PIGEON_ID, kSwerve.GYRO_INVERSION, kPorts.CANIVORE_NAME),
+            kSwerve.SWERVE_KINEMATICS,
+            kSwerve.MAX_SPEED,
+            kSwerve.SWERVE_TUNNING_MODE
+        );
 
         locationLock = false;
         locationLockPID = new PIDController(
@@ -63,9 +57,6 @@ public class Swerve extends SubsystemBase {
             0.0, 
             0.0
         );
-
-        field = new Field2d();
-        SmartDashboard.putData("Field", field);
 
         color = AllianceColor.getInstance();
     }
@@ -78,7 +69,7 @@ public class Swerve extends SubsystemBase {
         }
         
         locationLockPID.setSetpoint(angle);
-        locationLockPID.calculate(gyro.getAngle().getDegrees());
+        locationLockPID.calculate(getGyro().getAngle().getDegrees());
     }
 
     public void turnOfLocationLock() {
@@ -87,113 +78,14 @@ public class Swerve extends SubsystemBase {
 
     public void driveWithRotationLock(Translation2d translation, double rotation) {
         if (locationLock) {
-            rotation = locationLockPID.calculate(gyro.getAngle().getDegrees());
+            rotation = locationLockPID.calculate(getGyro().getAngle().getDegrees());
         }
 
         drive(translation, rotation);
     }
 
-    public void drive(ChassisSpeeds chassisSpeeds) {
-        driveRobotOriented(
-            new Translation2d(
-                // color.isRed() ? 1 : -1  * chassisSpeeds.vxMetersPerSecond, 
-                chassisSpeeds.vxMetersPerSecond,
-                // color.isRed() ? -1 : 1 * chassisSpeeds.vyMetersPerSecond
-                chassisSpeeds.vyMetersPerSecond
-            ),
-            chassisSpeeds.omegaRadiansPerSecond
-        );
-    }
-
-    public void driveRobotOriented(Translation2d vector, double rot) {
-        SwerveModuleState[] states = kSwerve.SWERVE_KINEMATICS.getStates(vector, rot);
-
-        // TODO: FIX SHITY CODE https://github.com/frc1678/C2023-Public/blob/main/src/main/java/com/team1678/lib/swerve/SwerveDriveKinematics.java
-        for (SwerveModuleState i : states) {
-            if (i.speedMetersPerSecond > kSwerve.MAX_SPEED) {
-                i.speedMetersPerSecond = kSwerve.MAX_SPEED;
-            }
-        }
-
-        for (SwerveModule i : swerveMods) {
-            if (kSwerve.SWERVE_TUNNING_MODE) {
-                SmartDashboard.putString("Swerve Module State " + i.moduleNumber, states[i.moduleNumber].speedMetersPerSecond + ", " + states[i.moduleNumber].angle.getDegrees());
-            }
-            i.setDesiredState(states[i.moduleNumber]);
-        }
-    }
-
-    // Vector is in mps, and rot is in radians per sec
-    public void drive(Translation2d vector, double rot) {
-        if (kSwerve.SWERVE_TUNNING_MODE) {
-            SmartDashboard.putString("Input: ", vector.getX() + ", " + vector.getY() + ", " + rot);
-        }
-
-        vector = vector.rotateBy(gyro.getAngle());
-
-        if (kSwerve.SWERVE_TUNNING_MODE) {
-            SmartDashboard.putString("Input Post Rotate : ", vector.getX() + ", " + vector.getY() + ", " + rot);
-        }
-
-        SwerveModuleState[] states = kSwerve.SWERVE_KINEMATICS.getStates(vector, rot);
-
-        // TODO: FIX SHITY CODE https://github.com/frc1678/C2023-Public/blob/main/src/main/java/com/team1678/lib/swerve/SwerveDriveKinematics.java
-        for (SwerveModuleState i : states) {
-            if (i.speedMetersPerSecond > kSwerve.MAX_SPEED) {
-                i.speedMetersPerSecond = kSwerve.MAX_SPEED;
-            }
-        }
-
-        for (SwerveModule i : swerveMods) {
-            if (kSwerve.SWERVE_TUNNING_MODE) {
-                SmartDashboard.putString("Swerve Module State " + i.moduleNumber, states[i.moduleNumber].speedMetersPerSecond + ", " + states[i.moduleNumber].angle.getDegrees());
-            }
-            i.setDesiredState(states[i.moduleNumber]);
-        }
-    }
-
-    public void updateEncoders() {
-        for (SwerveModule mod : swerveMods) {
-            mod.resetToAbsolute();
-        }
-    }
-
-    public SwerveModulePosition[] getPose() {
-        SwerveModulePosition[] ret = new SwerveModulePosition[]{null, null, null, null};
-
-        for (SwerveModule i : swerveMods) {
-            ret[i.moduleNumber] = i.getPose();
-        } 
-
-        return ret;
-    }
-
-    public void setOdomPose(Pose2d pose) { 
-        odom.setPose(pose);
-        gyro.setAngle(pose.getRotation());
-    }
-
-    public Pose2d getOdomPose() { return odom.getPose(); }
-
-    public Gyro getGyro() { return gyro; }
-
-    public void resetGyro() {
-        gyro.zeroGyro();
-    }
-
     @Override
     public void periodic() { 
-        odom.updatePoseWithGyro(getPose(),  gyro.getAngle());
-
-        SmartDashboard.putNumber("Angle", MathUtil.inputModulus(gyro.getAngle().getDegrees(), 0, 360));
-
-        field.setRobotPose(odom.getPose());
-
-        for (SwerveModule i : swerveMods) {
-            if (kSwerve.SWERVE_TUNNING_MODE) {
-                SmartDashboard.putNumber("Swerve Module Angle " + i.moduleNumber, i.getAngle());
-            }
-            i.log();
-        }
+        super.periodic();
     }
 }
